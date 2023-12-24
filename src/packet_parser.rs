d5bc1862c73be12c5aad44481b2798752c12d937
Mushow <antoine@mushow.uk>
2023-12-24 20:45:33 +0100

    Enhance packet parsing and frame information extraction

--- src/packet_parser.rs
+++ src/packet_parser.rs
@@ -23,13 +23,25 @@ fn get_type(metadata: &String) -> String {
     return packet_type;
 }
 
+fn get_tls(metadata: &String) -> String {
+    let mut tls_version = "TLS".to_string();
+
+    for line in metadata.lines() {
+        if line.trim().starts_with("TLSv1.") {
+            let tls_version = line.split_whitespace().next().unwrap_or_default();
+            return tls_version.to_string();
+        }
+    }
+
+    return tls_version;
+}
 
 pub fn get_frame_info(packet: &Packet) -> Frame {
     let metadata = parse_packet_data(&packet);
 
     Frame::new(
-        get_packet_metadata(&packet),
-        0,
+        get_packet_metadata(&metadata),
+        get_id(&metadata),
         get_time(&metadata),
         get_source(&metadata),
         get_destination(&metadata),
@@ -38,40 +50,69 @@ pub fn get_frame_info(packet: &Packet) -> Frame {
         get_info(&metadata))
 }
 
-pub fn get_packet_metadata(packet: &Packet) -> String {
-    packet
-        .iter()
-        .flat_map(|layer| layer.iter().map(|data| format!("{}\n", data.display())))
-        .collect()
-}
-fn get_time(metadata: &String) -> f64 {
-    //metadata.contains()
-    0.000000
+pub fn get_packet_metadata(metadata: &String) -> String {
+    metadata.parse().unwrap()
 }
 
-fn get_source(metadata: &String) -> String {
-    "".to_string()
-}
-fn get_destination(metadata: &String) -> String {
-    "".to_string()
-}
-
-fn get_protocol(metadata: &String) -> String {
-    let mut protocol = "Protocol not found".to_string();
-
+fn get_id(metadata: &String) -> usize {
+    let mut id = 0;
     for line in metadata.lines() {
-        if line.trim().starts_with("Protocols in frame:") {
-            if let Some(protocols) = line.split(':').last() {
-                protocol = protocols.trim().to_uppercase().to_string();
+        if line.trim().starts_with("Frame Number:") {
+            if let Some(types) = line.split(':').nth(1) {
+                id = types.trim().parse().unwrap();
             }
-            break;
+            return id;
         }
     }
 
-    let packet_type = get_type(metadata);
+    id
+}
 
-    if protocol == "DATA" && packet_type.contains("Unknown") {
-        protocol = "0x7373".to_string();
+fn get_time(metadata: &String) -> f64 {
+    let mut time = 0.0;
+
+    for line in metadata.lines() {
+        if line.trim().starts_with("Time since reference or first frame:") {
+            if let Some(_time_str) = line.split(':').nth(1) {
+                let numeric_str: String = _time_str.chars().filter(|&c| c.is_digit(10) || c == '.').collect();
+                if let Ok(parsed_value) = numeric_str.parse::<f64>() {
+                    time = parsed_value;
+                }
+            }
+        }
+    }
+
+    time
+}
+
+fn get_source(metadata: &String) -> String {
+    String::new()
+}
+fn get_destination(metadata: &String) -> String {
+    String::new()
+}
+
+fn get_protocol(metadata: &String) -> String {
+    let mut protocol = String::new();
+
+    for line in metadata.lines() {
+        if line.trim().starts_with("Protocols in frame:") {
+            let mut protocols: Vec<&str> = line.split(':').map(|s| s.trim()).collect();
+
+            if protocols.contains(&"data") && metadata.contains("Unknown") {
+                protocol = "0x7373".to_string();
+                return protocol;
+            } else {
+                protocols.retain(|&word| !word.contains("data") && !word.contains("mime"));
+                protocol = protocols.last().unwrap().to_uppercase().to_string();
+            }
+
+            if protocol == "TLS" {
+                protocol = get_tls(&metadata);
+            }
+
+            break;
+        }
     }
 
     protocol
@@ -82,5 +123,5 @@ fn get_length(metadata: &String) -> i32 {
 }
 
 fn get_info(metadata: &String) -> String {
-    "".to_string()
+    String::new()
 }
